42656a4620787626adf842483b163b9542983513
Edward Von <skyway123@foxmail.com>
2023-06-26 14:50:30 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-PLUGIN_DIR="/userdisk/appdata/2882303761518031252"
 PLUGIN_ID="2882303761518031252"
+PLUGIN_DIR=/userdisk/appdata/$PLUGIN_ID
 
-mkdir /data/uu && cd /data/uu
-# 如果下载一直出错，可以把压缩包下载了手动解压
-curl -O https://raw.githubusercontent.com/edwardvon/uuplugin-redmi-ax6000/main/uuplugin.tar.gz
-tar -xzvf uuplugin.tar.gz
+if [ ! -d $PLUGIN_DIR ]; then
+    cp -r "./${PLUGIN_ID}" $PLUGIN_DIR
+    chmod 777 "${PLUGIN_DIR}/*"
+fi
 
-cp -r "./${PLUGIN_ID}" "${PLUGIN_DIR}"
-cp "./${PLUGIN_ID}.json" "/userdisk/appdata/installPlugin/${PLUGIN_ID}.json"
-cd "${PLUGIN_DIR}"
-chmod 777 *
+if [ ! -f "/userdisk/appdata/installPlugin/${PLUGIN_ID}.json" ]; then
+    cp "./${PLUGIN_ID}.json" "/userdisk/appdata/installPlugin/${PLUGIN_ID}.json"
+fi
 
-echo "sleep 5 && /bin/sh ${PLUGIN_DIR}/uuplugin_monitor.sh &" >> /data/auto_ssh/auto_ssh.sh
-
-
+$PLUGIN_DIR/uuplugin_monitor.sh 
