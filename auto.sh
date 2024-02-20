e12a0da6dfa5a9ff0dd593e7a31cf77cc5a90dcb
Edward Von <skyway123@foxmail.com>
2024-02-20 21:24:15 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,8 +4,8 @@ PLUGIN_ID="2882303761518031252"
 PLUGIN_DIR=/userdisk/appdata/$PLUGIN_ID
 
 if [ ! -d $PLUGIN_DIR ]; then
+    chmod 777 ./$PLUGIN_ID/*
     cp -r "./${PLUGIN_ID}" $PLUGIN_DIR
-    chmod -R +x $PLUGIN_DIR
 fi
 
 if [ ! -f "/userdisk/appdata/installPlugin/${PLUGIN_ID}.json" ]; then
